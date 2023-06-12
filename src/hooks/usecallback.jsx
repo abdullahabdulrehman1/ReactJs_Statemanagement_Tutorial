de395502870ba452d186c172584e3475f078062f
abdullahabdulrehman1 <abdullahabdurrehman72@gmail.com>
2023-06-13 00:12:38 +0500

    update

---
+++ src/hooks/usecallback.jsx
@@ -0,0 +1,19 @@
+import React, { useCallback,useState,useMemo } from "react";
+
+function Sortedlist({list}){
+
+
+    const sortedlist = useMemo(()=>[...names].sort(),[list  ]);
+}
+function Usecallbackhook() {
+    
+  return <div>
+
+
+
+
+
+
+  </div>;
+}
+export default Usecallbackhook;
